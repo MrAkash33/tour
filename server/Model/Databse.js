72ef0ddf5ef2aa539df7d0f4b4639aa17e0667d2
Mr Akash <79826594+MrAkash33@users.noreply.github.com>
2022-10-07 13:22:43 +0530

    Add files via upload

---
+++ server/Model/Databse.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../../Congig.env' });
+
+    mongoose.connect('mongodb+srv://heyNik:<PASSWORD>g@cluster0.l0zap.mongodb.net/Tour?retryWrites=true&w=majority',
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+
+let db = mongoose.connection;
+
+db.on('error', error => {
+    console.error('Error in MongoDb connection: ' + error);
+      mongoose.disconnect();
+});
+
+db.on('connected', () => console.log('Tour Db connected'));
+    
